7406e8295989767e0ca21a53b224eaf722be3e9b
kimdh94 <44955958+kimdh94@users.noreply.github.com>
2022-10-23 18:12:38 +0900

    일자별 parquet 파일 생성 기능 추가
    csv 파일을 일자별로 파싱 작업하여 일자별 파일 생성
    생성된 일자별 csv파일을 parquet 변환 작업
    작업이 완료된 파일을 s3에 일자별로 업로드

--- main.py
+++ main.py
@@ -3,36 +3,95 @@ import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
 import sys
-import json, io
+import json, io, os
 from s3_conn import s3_connection
 from datetime import datetime
 
 
+# csv 파일 일자별 파싱하여 생성 후 일자별 리스트 반환
+def csv_parsing(obj, file_name, src_path, chunksize, header, dtype):
+    print("--- csv parsing start ---")
+    ymd_list =[]
+
+    csv_df = pd.read_csv(io.BytesIO(obj["Body"].read()), chunksize=chunksize, low_memory=False, names=header, dtype=dtype)
+
+    # 일자별로 파싱된 csv 파일 생성
+    for i, chunk in enumerate(csv_df):
+        print("parsing chunk", i)
+        # ymd 컬럼 추가 및 인덱스 설정
+        chunk['ymd'] = chunk['server_datetime'].astype(str).str[0:10]
+        chunk.set_index('ymd', inplace=True)
+        sort_chunk = chunk.sort_index()
+        
+        # 중복 제거를 위해 set -> list
+        chunk_ymd_list = list(set(sort_chunk.index.to_list()))
+        # 전체 리스트에 추가
+        ymd_list.extend(chunk_ymd_list)
+        
+        for dt in chunk_ymd_list:
+            # 일자별 dataframe 추출 및 저장
+            tmp_df = sort_chunk.loc[dt]
+            
+            # csv 파일명
+            tmp_file_name = file_name +'_' + dt
+
+            # 파일이 없을 경우 생성, 있을 경우 이어쓰기
+            if not os.path.exists(tmp_file_name):
+                tmp_df.to_csv(src_path+tmp_file_name, index=False, mode='w', header=False, quoting=1)
+            else:
+                tmp_df.to_csv(src_path+tmp_file_name, index=False, mode='a', header=False, quoting=1)
+
+    # 모든 일자 리스트
+    ymd_list = list(set(ymd_list))
+
+    print("--- csv parsing end ---")
+
+    return ymd_list
+
+
 # S3 - csv 파일 parquet 변환
-def csv_to_parquet(csv_file, parquet_file, upload_file, chunksize, header, dtype, parse_dates, parquet_schema):
+def csv_to_parquet(file_name, csv_file_path, src_path, parquet_file_path, upload_file_path, chunksize, header, dtype, parse_dates, parquet_schema):
     # S3 연결 및 객체 가져오기
     s3 = s3_connection()
     bucket_name = "igaworks-s3-bucket"
-    obj = s3.get_object(Bucket=bucket_name, Key=csv_file)
+    obj = s3.get_object(Bucket=bucket_name, Key=csv_file_path+file_name)
 
-    # csv 파일 읽기
-    # csv_stream = pd.read_csv(csv_file, chunksize=chunksize, low_memory=False, names=header, dtype=dtype, parse_dates=parse_dates)
+    # csv 파일 일자별 파싱 및 일자별 리스트 가져오기
+    ymd_list = csv_parsing(obj, file_name, src_path, chunksize, header, dtype)
+
     dt_parser = lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S.%f")
-    csv_stream = pd.read_csv(io.BytesIO(obj["Body"].read()), chunksize=chunksize, low_memory=False, names=header, dtype=dtype, parse_dates=parse_dates, date_parser=dt_parser)
+    
+    # 일자별 csv -> parquet 변환 및 s3 업로드
+    for ymd in ymd_list:
+        # src/(Attribution 또는 Event)/(Attribution 또는 Event)_yyyymmdd - 일별로 파싱한 csv 파일
+        src_file = src_path+file_name+'_'+ymd 
 
-    # parquet 변환
-    for i, chunk in enumerate(csv_stream):
-        print("Chunk", i)
-        if i == 0:
-            parquet_writer = pq.ParquetWriter(parquet_file, parquet_schema, compression='snappy')
-            
-        table = pa.Table.from_pandas(chunk, schema=parquet_schema)
-        parquet_writer.write_table(table)
+        # dest/(Attribution 또는 Event)/(Attribution 또는 Event)_yyyymmdd - 파싱된 csv 파일로 parquet 변환
+        parquet_file = parquet_file_path+file_name+'_'+ymd
 
-    parquet_writer.close()
+        # dest/(Attribution 또는 Event)/yyyymmdd/(Attribution 또는 Event)_yyyymmdd - S3 업로드 path
+        upload_file = upload_file_path+ymd+'/'+file_name+'_'+ymd
 
-    # parquet 파일 S3에 업로드
-    s3.upload_file(parquet_file, bucket_name, upload_file)
+        csv_stream = pd.read_csv(src_file, chunksize=chunksize, low_memory=False, names=header, dtype=dtype, parse_dates=parse_dates, date_parser=dt_parser)
+
+        # parquet 변환 작업
+        for i, chunk in enumerate(csv_stream):
+            print("Chunk", i)
+            if i == 0:
+                parquet_writer = pq.ParquetWriter(parquet_file, parquet_schema, compression='snappy')
+                
+            table = pa.Table.from_pandas(chunk, schema=parquet_schema)
+            parquet_writer.write_table(table)
+
+        parquet_writer.close()
+
+        # s3 업로드
+        s3.upload_file(parquet_file, bucket_name, upload_file)
+
+
+def dir_make(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
 
 
 def main():
@@ -61,16 +120,25 @@ def main():
         with open(file_path, encoding='UTF-8') as json_file:
             data = json.load(json_file)
 
-        csv_file = data["csv_file"]
-        parquet_file = data["parquet_file"]
-        upload_file = data["upload_file"]
+        file_name = data["file_name"]
+        csv_file_path = data["csv_file_path"]
+        parquet_file_path = data["parquet_file_path"]
+        upload_file_path = data["upload_file_path"]
         header = data["header"]
         dtype = data["dtype"]
         parse_dates = data["parse_dates"]
         chunksize = data["chunksize"]
 
+        # 필요한 디렉토리 생성
+        src_path = 'src/'+file_name+'/'
+        dest_path = 'dest/'+file_name+'/'
+
+        dir_make(src_path)
+        dir_make(dest_path)
+
+
         # 변환 함수 호출
-        csv_to_parquet(csv_file, parquet_file, upload_file, chunksize, header, dtype, parse_dates, parquet_schema)
+        csv_to_parquet(file_name, csv_file_path, src_path, parquet_file_path, upload_file_path, chunksize, header, dtype, parse_dates, parquet_schema)
 
         # 만들어진 parquet 데이터 조회
         # df = pd.read_parquet(parquet_file, engine='pyarrow')
